f4c5247690e793ff6ea40e848a5c575c859a0ca5
alesantoscoz <116589040+alesantoscoz@users.noreply.github.com>
2023-03-06 23:07:18 -0500

    validando path, validando ruta absoluta, convirtiendo ruta

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-module.exports = () => {
+/*module.exports = () => {
   // ...
-};
+};*/
+
+
+const fs = require('fs'); //file system
+const path = require('path'); //dirección (ruta)
+const direc = './txt-prueba.md' // archivo de prueba
+
+const validPath = (direc)=> fs.existsSync(direc); //validar que la dirección existe (devuelve booleano)
+console.log(validPath(direc)); //imprimir valor de verdad
+
+const pathAbs = (direc) => path.isAbsolute(direc);//validar si es absoluta o relativa (devuelve booleano)
+console.log(pathAbs(direc)); //imprimir valor de verdad
+
+ /*Creando función para transformar ruta relativa*/
+function turnPathAbs(){
+  if (pathAbs(direc)===false){
+   return path.join(__dirname,direc);
+  }
+  else{
+    return direc
+  }
+}
+console.log(turnPathAbs()); //imprimir ruta
+
+
+
