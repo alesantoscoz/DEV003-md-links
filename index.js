a17caa87907e273d7554ae5258d451808a8002d9
alesantoscoz <116589040+alesantoscoz@users.noreply.github.com>
2023-03-06 23:17:39 -0500

    agregando condicional ruta valida

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require('fs'); //file system
 const path = require('path'); //dirección (ruta)
 const direc = './txt-prueba.md' // archivo de prueba
 
-const validPath = (direc)=> fs.existsSync(direc); //validar que la dirección existe (devuelve booleano)
+const validPath = (direc) => fs.existsSync(direc); //validar que la dirección existe (devuelve booleano)
 console.log(validPath(direc)); //imprimir valor de verdad
 
 const pathAbs = (direc) => path.isAbsolute(direc);//validar si es absoluta o relativa (devuelve booleano)
@@ -15,12 +15,16 @@ console.log(pathAbs(direc)); //imprimir valor de verdad
 
  /*Creando función para transformar ruta relativa*/
 function turnPathAbs(){
-  if (pathAbs(direc)===false){
+  if(validPath(direc)===true){
+    if (pathAbs(direc)===false){
    return path.join(__dirname,direc);
-  }
-  else{
+    }
+    else{
     return direc
+    }
   }
+  else
+  return 'Inserte una ruta válida'
 }
 console.log(turnPathAbs()); //imprimir ruta
 
