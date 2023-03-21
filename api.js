21a117a3cd2d1238aed73869fa09d439cf22b5d7
alesantoscoz <116589040+alesantoscoz@users.noreply.github.com>
2023-03-21 14:18:28 -0500

    getmdLinks function (recursividad)

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const fs = require('fs'); //file system
 const path = require('path'); //dirección (ruta)
-const direc = 'C:/Users/51940/Desktop/LABORATORIA/PROYECTO_4/DEV003-md-links/carpeta' ;// direccion de prueba
+const direc = './carpeta' ;// direccion de prueba
 
 const validPath = (direc) => fs.existsSync(direc); //validar que la dirección existe
 const isAbs = (direc) => path.isAbsolute(direc);//validar si es absoluta o relativa
@@ -9,6 +9,7 @@ const readDir = (direc) => fs.readdirSync(direc); //Recorre la carpeta
 const isFile = (direc) => fs.statSync(direc).isFile(); //valida si es archivo
 const extMd = (direc) => path.extname(direc) === '.md'; //validar extensión de archivo
 const turningAbs = (direc) => path.join(__dirname, direc); //transformando a absoluta
+const readingFile = (direc) => fs.readFileSync(direc, 'utf8'); // lectura de archivo
 
 //Creando función para transformar ruta relativa
 function turningPathAbs(direc) {
@@ -17,7 +18,7 @@ function turningPathAbs(direc) {
       return turningAbs(direc); //convierte la ruta en absoluta
     }
     else { //si la ruta es absoluta
-      return direc
+      return direc //retorna la ruta absoluta
     }
   }
   else
@@ -40,77 +41,72 @@ function readingPath(direc) {
         return 'La ruta ingresada no existe. Por favor ingresa una ruta válida.'
 };
 
-const readingFile = (direc) => fs.readFileSync(direc, 'utf8');
-//console.log(readingFile(direc))
-
-let arrFiles = [];
 //consiguiendo array de archivos de una carpeta
-function getFiles(direc){
-    if(!isFile(direc)){ //si es folder
-        readDir(direc).forEach((file) => { 
-            const folder = direc+'/'+file;
-            //recursividad, vuelvo a llamar a la función para que lea cada folder hasta un archivo
-            getFiles(folder); 
-          });
-    }
-    else { //si es un file se agrega al array
-        return arrFiles.push(path.join(direc)); //se agrega directamente la ruta al array
-    };
-};
-getFiles(direc);
-//console.log(arrFiles);
-
-let arrmdFiles = [];
-
-function getmdFiles(){
-    arrmdFiles = arrFiles.filter((file=>extMd(file)));
-    return arrmdFiles
-};
-
-console.log(getmdFiles(direc));
-
-function getLinks(direc, arrLinks=[]){
+function getFiles(direc, arrFiles=[]){
     if(isDirectory(direc)){ //si es folder
         readDir(direc).forEach((file) => { 
             const folder = direc+'/'+file;
-            //recursividad, vuelvo a llamar a la función para que lea cada folder hasta un archivo
-            getLinks(folder,arrLinks); 
+            getFiles(folder, arrFiles); //recursividad, vuelvo a llamar a la función para que lea cada folder hasta un archivo
           });
-          return arrLinks;
     }
     else { //si es un file se agrega al array
-        const regex = /\[(.+)\]\((https?:\/\/\w+.+)\)/g; //expresión regular para identificar páginas web (preguntar si está ok)
-        const links = (readingFile(direc).match(regex));
-        if(links!=null){ // sólo agregaremos los archivos que tengan links
-            arrLinks.push(...(readingFile(direc).match(regex))); //crea un array con los links del archivo después de leerlo
-            //match devuelve las coincidencias con la expresión regular (regex)
-            //los tres puntos son para que netamente se sumen los objetos del array y no un array dentro de otro
-        }
-        return arrLinks;
+        arrFiles.push(turningPathAbs(direc)); //se agrega directamente la ruta al array
+        return arrFiles
     };
+    return arrFiles //devuelve el array con los archivos
 };
 
-const arrLinks = getLinks(direc)
+//filtrando array con sólo archivos md
+function getmdFiles(arrFiles=[]){
+    let arrmdFiles = arrFiles.filter((file=>extMd(file))); //filtra los archivos md del array con todos los archivos
+    return arrmdFiles //devuelve array con archivos md
+};
+
+//función para generar array con links
+function getLinks(filePath){
+    const regex = /\[(.+)\]\((https?:\/\/\w+.+)\)/g; //expresión regular para identificar páginas web (preguntar si está ok)
+    const links = (readingFile(filePath).match(regex)); //crea un array con los links del archivo después de leerlo
+    //match devuelve las coincidencias con la expresión regular (regex)
+    if(links!=null){ // sólo agregaremos los archivos que tengan links
+        return links //crea un array con los links del archivo después de leerlo
+    }
+    return []
+    
+};
 
 //crear arreglo href text file
-let arrObjetc=[];
-function getOb(){
-    for (let i = 0; i < arrLinks.length; i++){
+function getOb(direc){
+    let arrObjetc=[];
+    const arrLinks = getLinks(direc); //obtengo el array de links
+    //console.log(direc)
+    for (let i = 0; i < arrLinks.length; i++){ //recorrer cada elemento para extraer href, text y file
         arrObjetc.push({
             href: arrLinks[i].slice(arrLinks[i].indexOf('](')+2, -1),
             text: arrLinks[i].slice(1, arrLinks[i].indexOf('](')),
-            file: turningAbs(direc)
+            file: turningPathAbs(direc)
         });
     };
-    return arrObjetc;
+    return arrObjetc; //devuelve el arreglo
 };
-getOb(direc);
-console.log(arrObjetc);
 
-//console.log(mifuncion(x))
+// recursividad para obtener links de varios archivos
+function getmdLinks(direc){
+    let arrmdLinks =[];
+    let arrmdFiles = getmdFiles(getFiles(direc)) // se obtiene el arreglo de archivos del folder y se filtran los .md
+    for(let i = 0; i < arrmdFiles.length; i++){  // recorre cada uno de los archivos md del array
+        arrmdLinks.push(...getOb(arrmdFiles[i])); // suma a un nuevo arreglo hrf, text, file
+    }
+    return arrmdLinks
+}
+
+console.log(getmdLinks(direc))
+
+//creando función para validar links
+
 
  module.exports = {
     validPath,
     turningPathAbs,
-    readingPath
+    readingPath,
+    getLinks
   };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,33 +1,42 @@
 const { mdLinks } = require('../index.js');
 
-const { turningPathAbs, validPath  } = require('../api.js');
+const { turningPathAbs, validPath, readingPath, getLinks } = require('../api.js');
 
 
 describe('mdLinks', () => {
-
   it('Debería rechazar path inválido', () =>{
     return mdLinks('./estePathNoExiste.md').catch((error)=>{
       expect(error).toBe('La ruta ingresada no existe');
     })
   });
-
 });
 
 describe('validPath', () => {
-
   it('Debería ser una función', () => {
     expect(typeof validPath).toBe('function');
   });
   it('El valor de verdad', () => {
-    expect(validPath('./txt-prueba.md')).toEqual(true);
+    expect(validPath('./')).toEqual(true);
   });
 
 });
-
 
 describe('turningPathAbs', () => {
-
-  it('Debería ser una función', () => {
-    expect(typeof turningPathAbs).toBe('function');
+  it('Debería rechazar path invañido', () => {
+    expect(turningPathAbs('./estePathNoExiste.md')).toBe('La ruta ingresada no existe. Por favor ingresa una ruta válida.');
   });
 });
+
+describe('readingPath', () => {
+  it('Debería reconocer archivos .md', () => {
+    expect(readingPath('./txt-texto.txt')).toBe('El archivo no tiene extensión md');
+  });
+});
+
+describe('getLinks', () => {
+  it('Debería devolver un objeto', () => {
+    expect(typeof getLinks('./')).toBe('object');
+  });
+});
+
+
