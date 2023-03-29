27b125879e4319c15cb6198d1308da1bd3fb5652
alesantoscoz <116589040+alesantoscoz@users.noreply.github.com>
2023-03-28 23:35:03 -0500

    cli colors

--- cli.js
+++ cli.js
@@ -4,6 +4,7 @@ const { mdLinks } = require('./index.js');
 const api = require('./api');
 const process = require('process');
 const args = process.argv;
+const colors = require('colors');
 
 function cli() {
 
@@ -12,23 +13,47 @@ function cli() {
     const stats = args.includes('--stats') || args.includes('--s');
     const help = args.includes('--help') || args.includes('--h');
 
-    if(!path || help){
-        console.log('Bienvenido a tu librería mdLinks');
-        console.log('Primero introduce la ruta del archivo o folder para que podamos leerla.');
-        console.log('Seguido a ello, puedes usar los siguientes comandos:')
-        console.log('--validate: Te daremos un arreglo con los status de los links.');
-        console.log('--stats: Te brindaremos las estadísticas de los links totales y unicos.');
-        console.log('--validate --stats: Te brindaremos las estadísticas de los links rotos.');
+    if (!path || help) {
+        console.log(colors.brightMagenta.bold('Bienvenido a tu librería mdLinks'));
+        console.log(colors.brightGreen.bold('*********************************'))
+        console.log('mdLinks es una librería que te permite verificar los status de las URL en tus archivos .md .\n');
+        console.log('Primero introduce la ruta del archivo o folder para que podamos leerlo y extraer los links.\n');
+        console.log('Seguido a ello, puedes usar los siguientes comandos:\n')
+        console.log(colors.brightCyan('--validate o --v:               Te daremos un arreglo con los status de los links.'));
+        console.log(colors.brightCyan('--stats o --s:                  Te brindaremos las estadísticas de los links totales y unicos.'));
+        console.log(colors.brightCyan('--validate --stats o --v --s:   Te brindaremos las estadísticas de los links rotos.'));
     }
     else if (!validate && !stats) {
         return mdLinks(path, { validate: false }).then((object) => {
-            console.log(object);
+            for (let i = 0; i < object.length; i++) {
+                console.log(colors.brightCyan('***************************************************************************'));
+                console.log(colors.brightMagenta('HREF  :', object[i].href));
+                console.log('TEXT  :', object[i].text);
+                console.log('FILE  :', object[i].file);
+            }
             process.exit(0);
         }).catch(console.error)
     }
     else if (validate && !stats) {
         return mdLinks(path, { validate: validate }).then((object) => {
-            console.log(object);
+            for (let i = 0; i < object.length; i++) {
+                if (object[i].ok === 'fail') {
+                    console.log(colors.brightCyan('***************************************************************************'));
+                    console.log(colors.brightMagenta('HREF:', object[i].href));
+                    console.log('TEXT   :', object[i].text);
+                    console.log('FILE   :', object[i].file);
+                    console.log(colors.red('STATUS :', object[i].status));
+                    console.log(colors.red('OK     :', object[i].ok));
+                }
+                else {
+                    console.log(colors.brightCyan('***************************************************************************'));
+                    console.log(colors.brightMagenta('HREF:', object[i].href));
+                    console.log('TEXT   :', object[i].text);
+                    console.log('FILE   :', object[i].file);
+                    console.log(colors.green('STATUS :', object[i].status));
+                    console.log(colors.green('OK     :', object[i].ok));
+                }
+            }
             process.exit(0);
         }).catch(console.error);
     }
@@ -36,8 +61,8 @@ function cli() {
         return mdLinks(path, { validate: stats }).then((object) => {
             const totalLinks = api.totalLinks(object);
             const uniqueLinks = api.uniqueLinks(object);
-            console.log(totalLinks)
-            console.log(uniqueLinks);
+            console.log(colors.cyan.bold(totalLinks));
+            console.log(colors.cyan.bold(uniqueLinks));
             process.exit(0);
         }).catch(console.error);
     }
@@ -46,9 +71,9 @@ function cli() {
             const totalLinks = api.totalLinks(object);
             const uniqueLinks = api.uniqueLinks(object);
             const brokenLinks = api.brokenLinks(object);
-            console.log(totalLinks);
-            console.log(uniqueLinks);
-            console.log(brokenLinks);
+            console.log(colors.cyan.bold(totalLinks));
+            console.log(colors.cyan.bold(uniqueLinks));
+            console.log(colors.red.bold(brokenLinks));
             process.exit(0);
         }).catch(console.error);
     }
